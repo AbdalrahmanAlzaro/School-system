0cbc47f3837332253212242dfd4eacad312677b5
AbdalrahmanAlzaro <abdalrahman.h.alzaro@gmail.com>
2023-04-03 00:28:24 +0300

    add code

--- higherOrderFunction.js
+++ higherOrderFunction.js
@@ -84,3 +84,101 @@
 //   }
 //   console.log(fizzbuzz(arr));
   
+
+
+
+// function factorial(n) {
+//     if (n === 0) {
+//       return 1;
+//     } else {
+//       return n * factorial(n - 1);
+//     }
+//   }
+  
+//   console.log(factorial(5)); // Output: 120
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //Q2
+//   const queue = [1, 2, 3, 4, 5];
+//   function reverseQueue(queue) {
+//     const stack = [];
+//     while (queue.length > 0) {
+//       stack.push(queue.shift());
+//     }
+//     while (stack.length > 0) {
+//       queue.push(stack.pop());
+//     }
+//     return queue;
+//   }
+
+
+
+// reverseQueue(queue);
+// console.log(queue); // [5, 4, 3, 2, 1]
+
+  
+
+
+
+
+//Q3
+
+class Queue {
+  constructor() {
+    this.s1 = [];
+    this.s2 = [];
+  }
+
+  enqueue(val) {
+    this.s1.push(val);
+  }
+
+  dequeue() {
+    if (this.s2.length === 0) {
+      while (this.s1.length > 0) {
+        this.s2.push(this.s1.pop());
+      }
+    }
+    if (this.s2.length === 0) {
+      return null; // or throw an error
+    }
+    return this.s2.pop();
+  }
+}
+
+
+const q = new Queue();
+
+q.enqueue(1);
+q.enqueue(2);
+q.enqueue(3);
+
+console.log(q.dequeue()); // 1
+console.log(q.dequeue()); // 2
+console.log(q.dequeue()); // 3
+console.log(q.dequeue()); // null
