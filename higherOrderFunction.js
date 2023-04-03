11b0e1129078a7316d9874416d95c934f18822b3
AbdalrahmanAlzaro <abdalrahman.h.alzaro@gmail.com>
2023-04-03 23:20:23 +0300

    add

--- higherOrderFunction.js
+++ higherOrderFunction.js
@@ -146,39 +146,169 @@
 
 
 
-//Q3
+// //Q3
 
-class Queue {
-  constructor() {
-    this.s1 = [];
-    this.s2 = [];
+// class Queue {
+//   constructor() {
+//     this.s1 = [];
+//     this.s2 = [];
+//   }
+
+//   enqueue(val) {
+//     this.s1.push(val);
+//   }
+
+//   dequeue() {
+//     if (this.s2.length === 0) {
+//       while (this.s1.length > 0) {
+//         this.s2.push(this.s1.pop());
+//       }
+//     }
+//     if (this.s2.length === 0) {
+//       return null; // or throw an error
+//     }
+//     return this.s2.pop();
+//   }
+// }
+
+
+// const q = new Queue();
+
+// q.enqueue(1);
+// q.enqueue(2);
+// q.enqueue(3);
+
+// console.log(q.dequeue()); // 1
+// console.log(q.dequeue()); // 2
+// console.log(q.dequeue()); // 3
+// console.log(q.dequeue()); // null
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// linked list 
+
+
+
+// // q1
+// class Node {
+//   constructor(data) {
+//     this.data = data;
+//     this.next = null;
+//   }
+// }
+
+// class LinkedList {
+//   constructor() {
+//     this.head = null;
+//     this.tail = null;
+//     this.length = 0;
+//   }
+
+//   addNode(data) {
+//     const new_node = new Node(data);
+//     if (!this.head) {
+//       this.head = new_node;
+//       this.tail = this.head;
+//     } else {
+//       this.tail.next = new_node;
+//       this.tail = new_node;
+//     }
+//     this.length++;
+//   }
+
+
+//   deleteMiddleNode() {
+//     if (this.length < 2) {
+//       return this.head;
+//     }
+//     const middle = Math.floor(this.length / 2);
+//     let prevNode = null;
+//     let currNode = this.head;
+//     for (let i = 0; i < middle; i++) {
+//       prevNode = currNode;
+//       currNode = currNode.next;
+//     }
+//     prevNode.next = currNode.next;
+//     this.length--;
+//     return this.head;
+//   }
+
+
+// }
+
+
+// const linkedList = new LinkedList();
+// linkedList.addNode(1);
+// linkedList.addNode(2);
+// linkedList.addNode(3);
+// linkedList.addNode(4);
+// linkedList.addNode(5);
+
+ 
+// // Before deleting middle node
+// console.log(linkedList);
+
+// linkedList.deleteMiddleNode();
+
+// // After deleting middle node
+// console.log(linkedList); 
+
+
+
+
+
+
+
+// q2 
+class ListNode {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
   }
 
-  enqueue(val) {
-    this.s1.push(val);
-  }
-
-  dequeue() {
-    if (this.s2.length === 0) {
-      while (this.s1.length > 0) {
-        this.s2.push(this.s1.pop());
-      }
-    }
-    if (this.s2.length === 0) {
-      return null; // or throw an error
-    }
-    return this.s2.pop();
-  }
 }
 
+class LinkedList {
+    constructor(head = null) {
+        this.head = head;
+    } 
 
-const q = new Queue();
 
-q.enqueue(1);
-q.enqueue(2);
-q.enqueue(3);
 
-console.log(q.dequeue()); // 1
-console.log(q.dequeue()); // 2
-console.log(q.dequeue()); // 3
-console.log(q.dequeue()); // null
+      reverse(node1) {
+        var prev = null;
+        var current = node1;
+        var next = null;
+            while (current != null) {
+                next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+            }
+            node1 = prev;
+            return node1;
+        }
+        }
+let node1 = new LinkedList(1);
+let node2 = new LinkedList(2);
+let node3 = new LinkedList(3);
+
+node1.next = node2;
+node2.next = node3;
+
+let list = new LinkedList(node1);
+
+console.log(list);
+
+console.log(list.reverse(node1));
